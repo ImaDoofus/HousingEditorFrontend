4ca1303c6311ad0ef63bbb1e4458de2be353769d
ImaDoofus <ImaDoofusyt@gmail.com>
2022-07-10 01:35:06 -0600

    tags alignment

--- src/components/actions/ActionCard.vue
+++ src/components/actions/ActionCard.vue
@@ -21,12 +21,12 @@
 						<v-col cols="10" class="my-0 py-0">
 							<span>{{ action.post.title }}</span>
 						</v-col>
-						<v-col cols="12" class="my-0 py-0">
-							<router-link :to="`profile/${ action.author.id }`" class="user-router-link">@{{ action.author.name }}</router-link>
-							<span class="mx-1 overline">• {{ getUploadDate() }}</span>
+						<v-col class="my-0 py-0 d-flex align-start" style="height: 14px;" fluid>
+							<router-link :to="`profile/${ action.author.id }`" class="user-router-link" style="line-height: 14px;">@{{ action.author.name }}</router-link>
+							<span class="mx-1 overline" style="line-height: 14px;">• {{ getUploadDate() }}</span>
 						</v-col>
-						<v-col class="my-0 py-0 d-flex align-start" style="height: 16px;">
-							<v-chip :class='`mr-1 py-0 my-0 ${getTagTextColor(tag)}--text`' x-small v-for="tag in action.post.tags" :key="tag" :color="getTagColor(tag)"
+						<v-col cols="12" class="my-1 py-0 d-flex align-start" style="line-height: 16px;">
+							<v-chip :class='`mr-1 my-0 py-1 ${getTagTextColor(tag)}--text`' x-small v-for="tag in action.post.tags" :key="tag" :color="getTagColor(tag)"
 								style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
 								{{ tag }}
 							</v-chip>
